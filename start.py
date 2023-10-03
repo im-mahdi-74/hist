6dbb52ef4cb74266d3d866faa07e0fd2e4889e80
im_mahdi_74 <88947901+im-mahdi-74@users.noreply.github.com>
2023-10-03 23:23:51 +0000

    add session for handel any users

--- start.py
+++ start.py
@@ -2,22 +2,25 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-st.set_page_config(
 
-    page_title = ' !پنل انالیز  ' 
+from streamlit import session_state as session
 
-)
+# تابع مدیریت session
+def session_manager():
+  if "username" not in session:
+    session.username = st.text_input("Enter username")  
 
 st.title('صفحه اصلی')
-st.sidebar.success('!صفحه مد نظر خود را باز کنید')
+
+session_manager()
+
+st.write(f"Welcome {session.username}!") 
 
 
-st.warning(' دوستان اینجا درست شده تابتونیم سابقه های معاملاتی رو برسی و شبیه سازی کنیم ')
 
+st.warning('دوستان اینجا درست شده تابتونیم سابقه های معاملاتی رو برسی و شبیه سازی کنیم ')
 
 image = Image.open('mamosh.jpg')
 
 st.image(image, caption='زبور عسل')
-
-
-
+st.title('زنبور عسل')

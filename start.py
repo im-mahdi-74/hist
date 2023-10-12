795960a70f74869b048ab0b48c50aa2b692d7adb
im_mahdi_74 <88947901+im-mahdi-74@users.noreply.github.com>
2023-10-12 16:23:23 +0330

    Update start.py
    non

--- start.py
+++ start.py
@@ -3,16 +3,9 @@ import pandas as pd
 from PIL import Image
 
 
-from streamlit import session_state as session
-
-# تابع مدیریت session
-def session_manager():
-  if "username" not in session:
-    session.username = st.text_input("Enter username")  
-
 st.title('صفحه اصلی')
 
-session_manager()
+
 
 st.write(f"Welcome {session.username}!") 
 

ed1fd947ff33d2cb83d918200ae976721f57769d
im_mahdi_74 <88947901+im-mahdi-74@users.noreply.github.com>
2023-10-12 16:30:46 +0330

    b
    to b

--- start.py
+++ start.py
@@ -3,17 +3,19 @@ import pandas as pd
 from PIL import Image
 
 
+
+
+
+
 st.title('صفحه اصلی')
 
-
-
-st.write(f"Welcome !") 
-
-
-
 st.warning('دوستان اینجا درست شده تابتونیم سابقه های معاملاتی رو برسی و شبیه سازی کنیم ')
+
 
 image = Image.open('mamosh.jpg')
 
 st.image(image, caption='زبور عسل')
+
+
 st.title('زنبور عسل')
+

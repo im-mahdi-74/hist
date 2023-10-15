6e44b024572c88565ba92099d28421ac42aafb2c
im_mahdi_74 <88947901+im-mahdi-74@users.noreply.github.com>
2023-10-15 18:56:41 +0330

    add main class
    config in start page

--- start.py
+++ start.py
@@ -2,20 +2,30 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+def main():
+    st.title('صفحه اصلی')
+
+    st.warning('دوستان اینجا درست شده تابتونیم سابقه های معاملاتی رو برسی و شبیه سازی کنیم ')
 
 
+    image = Image.open('mamosh.jpg')
+    st.title('زنبور عسل')
+    st.image(image, caption='زبور عسل')
 
 
-
-st.title('صفحه اصلی')
-
-st.warning('دوستان اینجا درست شده تابتونیم سابقه های معاملاتی رو برسی و شبیه سازی کنیم ')
+    
 
 
-image = Image.open('mamosh.jpg')
+if __name__=='__main__':
 
-st.image(image, caption='زبور عسل')
+    st.set_page_config(
+
+        page_title= 'Anal Hist',
+        page_icon= 'hot'
+        #layout= 'wide'
+        )
+
+    main()
 
 
-st.title('زنبور عسل')
 
